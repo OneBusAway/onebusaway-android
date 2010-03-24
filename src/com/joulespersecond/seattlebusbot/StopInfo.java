7f9dc023ebaefef863ae6085f2ffd1fd002318df
Paul Watts <paulcwatts@gmail.com>
2010-03-24 16:31:46 +0000

    String changes and R.plurals

--- src/com/joulespersecond/seattlebusbot/StopInfo.java
+++ src/com/joulespersecond/seattlebusbot/StopInfo.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 
 import android.content.Context;
+import android.content.res.Resources;
 
 import com.joulespersecond.oba.ObaArray;
 import com.joulespersecond.oba.ObaArrivalInfo;
@@ -17,8 +18,8 @@ final class StopInfo {
     }
 
     public static final ArrayList<StopInfo>
-    convertObaArrivalInfo(Context context, 
-              ObaArray<ObaArrivalInfo> arrivalInfo, 
+    convertObaArrivalInfo(Context context,
+              ObaArray<ObaArrivalInfo> arrivalInfo,
               ArrayList<String> filter) {
         final int len = arrivalInfo.length();
         ArrayList<StopInfo> result = new ArrayList<StopInfo>(len);
@@ -29,14 +30,14 @@ final class StopInfo {
                 if (filter.contains(arrival.getRouteId())) {
                     result.add(new StopInfo(context, arrival, ms));
                 }
-            }            
+            }
         }
         else {
             for (int i=0; i < len; ++i) {
                 result.add(new StopInfo(context, arrivalInfo.get(i), ms));
             }
         }
-        
+
         // Sort by ETA
         Collections.sort(result, new StopInfoComparator());
         return result;
@@ -47,9 +48,9 @@ final class StopInfo {
     private final long mDisplayTime;
     private final String mStatusText;
     private final int mColor;
-    
+
     private static final int ms_in_mins = 60*1000;
-    
+
     public StopInfo(Context context, ObaArrivalInfo info, long now) {
         mInfo = info;
         // First, all times have to have to be converted to 'minutes'
@@ -58,78 +59,65 @@ final class StopInfo {
         final long predicted = info.getPredictedArrivalTime();
         final long scheduledMins = scheduled/ms_in_mins;
         final long predictedMins = predicted/ms_in_mins;
-        
+        final Resources res = context.getResources();
+
         if (predicted != 0) {
             mEta = predictedMins - nowMins;
             mDisplayTime = predicted;
-            final long delay = predictedMins - scheduledMins;
-            
+            long delay = predictedMins - scheduledMins;
+
             if (mEta >= 0) {
                 // Bus is arriving
                 if (delay > 0) {
                     // Arriving delayed
                     mColor = R.color.stop_info_delayed;
-                    if (delay == 1) {
-                        mStatusText = context.getString(R.string.stop_info_arrive_delayed1);                            
-                    }
-                    else {
-                        mStatusText = context.getString(R.string.stop_info_arrive_delayed, delay);                    
-                    }
+                    mStatusText = res.getQuantityString(R.plurals.stop_info_arrive_delayed,
+                    							(int)delay, delay);
                 }
                 else if (delay < 0) {
                     // Arriving early
                     mColor = R.color.stop_info_early;
-                    if (delay == -1) {
-                        mStatusText = context.getString(R.string.stop_info_arrive_early1);                            
-                    }
-                    else {
-                        mStatusText = context.getString(R.string.stop_info_arrive_early, -delay);                       
-                    }
+                    delay = -delay;
+                    mStatusText = res.getQuantityString(R.plurals.stop_info_arrive_early,
+                    							(int)delay, delay);
                 }
                 else {
                     // Arriving on time
                     mColor = R.color.stop_info_ontime;
                     mStatusText = context.getString(R.string.stop_info_ontime);
                 }
-            } 
+            }
             else {
                 // Bus is departing
                 if (delay > 0) {
                     // Departing delayed
                     mColor = R.color.stop_info_delayed;
-                    if (delay == 1) {
-                        mStatusText = context.getString(R.string.stop_info_depart_delayed1);                            
-                    }
-                    else {
-                        mStatusText = context.getString(R.string.stop_info_depart_delayed, delay);                        
-                    }
-                } 
+                    mStatusText = res.getQuantityString(R.plurals.stop_info_depart_delayed,
+                    							(int)delay, delay);
+                }
                 else if (delay < 0) {
                     // Departing early
                     mColor = R.color.stop_info_early;
-                    if (delay == -1) {
-                        mStatusText = context.getString(R.string.stop_info_depart_early1);                            
-                    }
-                    else {
-                        mStatusText = context.getString(R.string.stop_info_depart_early, -delay);                        
-                    }
+                    delay = -delay;
+                    mStatusText = res.getQuantityString(R.plurals.stop_info_depart_early,
+                    							(int)delay, delay);
                 }
                 else {
                     // Departing on time
                     mColor = R.color.stop_info_ontime;
                     mStatusText = context.getString(R.string.stop_info_ontime);
                 }
-            }                
+            }
         }
         else {
             mColor = R.color.stop_info_ontime;
-            
+
             mEta = scheduledMins - nowMins;
             mDisplayTime = scheduled;
             if (mEta > 0) {
                 mStatusText = context.getString(R.string.stop_info_scheduled_arrival);
             } else {
-                mStatusText = context.getString(R.string.stop_info_scheduled_departure);                    
+                mStatusText = context.getString(R.string.stop_info_scheduled_departure);
             }
         }
     }

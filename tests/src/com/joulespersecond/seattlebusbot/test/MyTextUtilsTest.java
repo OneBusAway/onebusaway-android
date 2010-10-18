bf917fb90ce5e470ccc6f4a85f6451718a900190
Paul Watts <paulcwatts@gmail.com>
2010-10-18 03:39:21 +0000

    Slight change to title casing
    Passing null to toTitleCase() returns null. This makes adding it
    to some previous logic easier, and fixes a bug where sometimes
    there is a null stop title...

--- tests/src/com/joulespersecond/seattlebusbot/test/MyTextUtilsTest.java
+++ tests/src/com/joulespersecond/seattlebusbot/test/MyTextUtilsTest.java
@@ -7,6 +7,7 @@ import android.test.AndroidTestCase;
 public class MyTextUtilsTest extends AndroidTestCase {
 
     public void testTitleCase() {
+        assertEquals(null, MyTextUtils.toTitleCase(null));
         assertEquals("E John St & 13th Ave E", MyTextUtils.toTitleCase("E JOHN ST & 13th AVE E"));
         assertEquals("Seattle", MyTextUtils.toTitleCase("SEATTLE"));
         assertEquals("Seattle", MyTextUtils.toTitleCase("Seattle"));

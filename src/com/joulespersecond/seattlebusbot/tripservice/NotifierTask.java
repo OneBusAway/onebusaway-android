61bea2591938b99d3cdfdf15e221f57d01ee7bb5
Paul Watts <paulcwatts@gmail.com>
2012-06-20 19:55:19 -0700

    Clear notification deprecation warning (one of them, at least)
    This uses the compatibility library for building notifications. I'm not
    exactly sure just yet how to clear the other one.

--- src/com/joulespersecond/seattlebusbot/tripservice/NotifierTask.java
+++ src/com/joulespersecond/seattlebusbot/tripservice/NotifierTask.java
@@ -30,6 +30,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
 import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
 
 /**
  *
@@ -109,31 +110,30 @@ public final class NotifierTask implements Runnable {
         mTaskContext.setNotification(id, notification);
     }
 
+    private static final long[] VIBRATE_PATTERN = {
+        0,    // on
+        1000, // off
+        1000, // on
+        1000, // off
+        1000, // on
+        1000, // off
+    };
+
     private Notification createNotification(Uri alertUri) {
         //Log.d(TAG, "Creating notification for alert: " + alertUri);
-        Notification notification = new Notification(R.drawable.ic_stat_notification, null,
-                System.currentTimeMillis());
-        notification.defaults = Notification.DEFAULT_SOUND;
-        notification.flags = Notification.FLAG_ONLY_ALERT_ONCE
-                | Notification.FLAG_SHOW_LIGHTS;
-        notification.ledOnMS = 1000;
-        notification.ledOffMS = 1000;
-        notification.ledARGB = 0xFF00FF00;
-        notification.vibrate = new long[] {
-                0,    // on
-                1000, // off
-                1000, // on
-                1000, // off
-                1000, // on
-                1000, // off
-        };
-
         Intent deleteIntent = new Intent(mContext, TripService.class);
         deleteIntent.setAction(TripService.ACTION_CANCEL);
         deleteIntent.setData(alertUri);
-        notification.deleteIntent = PendingIntent.getService(mContext, 0,
-                deleteIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-        return notification;
+
+        return new NotificationCompat.Builder(mContext)
+            .setSmallIcon(R.drawable.ic_stat_notification)
+            .setDefaults(Notification.DEFAULT_SOUND)
+            .setOnlyAlertOnce(true)
+            .setLights(0xFF00FF00, 1000, 1000)
+            .setVibrate(VIBRATE_PATTERN)
+            .setDeleteIntent(PendingIntent.getService(mContext, 0,
+                    deleteIntent, PendingIntent.FLAG_UPDATE_CURRENT))
+            .getNotification();
     }
 
     private void setLatestInfo(Notification notification,

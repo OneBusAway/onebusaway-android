70c7d5f646343075c665ee046b5615fc806d91fd
Sean Barbeau <barbeau@cutr.usf.edu>
2014-11-26 09:47:10 -0500

    Remove reference to list view

--- onebusaway-android/src/main/java/org/onebusaway/android/ui/NavigationDrawerFragment.java
+++ onebusaway-android/src/main/java/org/onebusaway/android/ui/NavigationDrawerFragment.java
@@ -40,7 +40,6 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -127,8 +126,6 @@ public class NavigationDrawerFragment extends Fragment {
     private DrawerLayout mDrawerLayout;
 
     private ViewGroup mDrawerItemsListContainer;
-
-    private ListView mDrawerListView;
 
     private View mFragmentContainerView;
 

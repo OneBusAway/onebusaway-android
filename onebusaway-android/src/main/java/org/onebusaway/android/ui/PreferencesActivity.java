d60ce77ba4e4f604a662df67a40d28ec6f71cac4
Sean Barbeau <sjbarbeau@gmail.com>
2015-06-29 17:46:11 -0400

    Fix #258 - Add "Action Bar" and Back button to Preferences Activity

--- onebusaway-android/src/main/java/org/onebusaway/android/ui/PreferencesActivity.java
+++ onebusaway-android/src/main/java/org/onebusaway/android/ui/PreferencesActivity.java
@@ -28,6 +28,7 @@ import org.onebusaway.android.util.BuildFlavorConstants;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.preference.ListPreference;
 import android.preference.Preference;
@@ -35,9 +36,16 @@ import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceCategory;
 import android.preference.PreferenceScreen;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.TypedValue;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class PreferencesActivity extends PreferenceActivity
@@ -130,6 +138,51 @@ public class PreferencesActivity extends PreferenceActivity
     protected void onStart() {
         super.onStart();
         ObaAnalytics.reportActivityStart(this);
+    }
+
+
+    /**
+     * Imitate Action Bar with back button - from http://stackoverflow.com/a/27455363/937715
+     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        Toolbar bar;
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent()
+                    .getParent().getParent();
+            bar = (Toolbar) LayoutInflater.from(this)
+                    .inflate(R.layout.settings_toolbar, root, false);
+            root.addView(bar, 0); // insert at top
+        } else {
+            // For Gingerbread
+            ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+            ListView content = (ListView) root.getChildAt(0);
+            root.removeAllViews();
+
+            bar = (Toolbar) LayoutInflater.from(this)
+                    .inflate(R.layout.settings_toolbar, root, false);
+
+            int height;
+            TypedValue tv = new TypedValue();
+            if (getTheme().resolveAttribute(R.attr.actionBarSize, tv, true)) {
+                height = TypedValue
+                        .complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
+            } else {
+                height = bar.getHeight();
+            }
+            content.setPadding(0, height, 0, 0);
+            root.addView(content);
+            root.addView(bar);
+        }
+
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     @Override

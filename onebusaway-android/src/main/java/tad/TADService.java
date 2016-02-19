923b15d6be16538ef1e87a87ddfdd4fbbcb87793
Aziz Batihk <aziz@azizmb.com>
2016-02-19 12:54:29 -0500

    Removed ProxProvider for now.

--- onebusaway-android/src/main/java/tad/TADService.java
+++ onebusaway-android/src/main/java/tad/TADService.java
@@ -1,15 +1,19 @@
 package tad;
 
-import android.app.IntentService;
 import android.app.NotificationManager;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
-import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.preference.ListPreference;
+import android.preference.Preference;
+import android.preference.Preference.OnPreferenceChangeListener;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceCategory;
+import android.preference.PreferenceScreen;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
@@ -46,7 +50,6 @@ public class TADService extends Service
         this.dLocation.setLatitude(intent.getDoubleExtra("STOP_LAT", 0));
         this.dLocation.setLongitude(intent.getDoubleExtra("STOP_LNG", 0));
 
-        final ProximityProvider provider = new ProximityProvider();
         mLocationHelper = new LocationHelper(this);
 
         if (mLocationHelper != null) {

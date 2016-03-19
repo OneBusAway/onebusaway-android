5168335932776abf58a4af989f3103ea2871bf25
Aziz Batihk <aziz@azizmb.com>
2016-03-18 23:41:09 -0400

    Added support for different actions.

--- onebusaway-android/src/main/java/org/onebusaway/android/tad/TripReceiver.java
+++ onebusaway-android/src/main/java/org/onebusaway/android/tad/TripReceiver.java
@@ -4,6 +4,7 @@ import android.app.NotificationManager;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.speech.tts.TextToSpeech;
 import android.util.Log;
 
 import org.onebusaway.android.app.Application;
@@ -12,16 +13,35 @@ import org.onebusaway.android.app.Application;
  * Created by azizmb on 3/16/16.
  */
 public class TripReceiver extends BroadcastReceiver {
+    public static final String TAG = "BroadcastReceiver";
+
     public static final String NAV_ID = ".NAV_ID";
+    public static final String ACTION_NUM = ".ACTION_NUM";
+
+    public static final int DISMISS_NOTIFICATION = 1;
+    public static final int CANCEL_TRIP = 2;
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Log.i("BR", "Received.");
         int navId = intent.getIntExtra(NAV_ID, 0);
+        switch (intent.getIntExtra(ACTION_NUM, 0)) {
+            case DISMISS_NOTIFICATION:
+
+                break;
+
+            case CANCEL_TRIP:
+                cancelTrip(navId);
+                break;
+        }
+
+    }
+
+    private void cancelTrip(int navId)
+    {
         Context appCxt = Application.get().getApplicationContext();
         appCxt.stopService(new Intent(appCxt, TADService.class));
         NotificationManager manager = (NotificationManager)
-                context.getSystemService(Context.NOTIFICATION_SERVICE);
+                appCxt.getSystemService(Context.NOTIFICATION_SERVICE);
         manager.cancel(TADNavigationServiceProvider.NOTIFICATION_ID);
     }
 

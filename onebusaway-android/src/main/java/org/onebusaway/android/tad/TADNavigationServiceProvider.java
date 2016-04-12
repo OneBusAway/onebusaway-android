5e992ceb351486f3f33ef7d527ec4f0e07adc76f
Aziz Batihk <aziz@azizmb.com>
2016-04-11 22:55:12 -0400

    NavServProvider now runs sequentially instead of on thread.

--- onebusaway-android/src/main/java/org/onebusaway/android/tad/TADNavigationServiceProvider.java
+++ onebusaway-android/src/main/java/org/onebusaway/android/tad/TADNavigationServiceProvider.java
@@ -32,7 +32,7 @@ import java.util.Locale;
  *
  * @author Barbeau / Belov
  */
-public class TADNavigationServiceProvider implements Runnable, TextToSpeech.OnInitListener {
+public class TADNavigationServiceProvider implements TextToSpeech.OnInitListener {
 
     public static final String TAG = "TADNavServiceProvider";
     public static final int NOTIFICATION_ID = 33620;
@@ -332,8 +332,7 @@ public class TADNavigationServiceProvider implements Runnable, TextToSpeech.OnIn
      */
     public void locationUpdated(Location l) {
         currentLocation = l;
-        Thread thread = new Thread(this);
-        thread.start();
+        mProxListener.checkProximityAll(currentLocation);
     }
 
     /**
@@ -352,35 +351,6 @@ public class TADNavigationServiceProvider implements Runnable, TextToSpeech.OnIn
 
 
     private int sendCounter = 0;
-
-    public void run() {
-
-        try {
-
-            //Get the last known location from location listener and send it to proximity listener.
-
-            //sends UDP data to server - Duplicated in LocListener
-          /*  JSR179LocationData loc = new JSR179LocationData(this.locListener.getLocationProvider(), candidate);
-            this.communicator.sendData(z);  //Send location data to the server   
-             */
-            int loc = mProxListener.checkProximityAll(currentLocation);
-
-            //if ((sendCounter++ % 4 == 0) || (loc.getAlert() == 1) || (loc.getAlert() == 2)) {
-            if (loc == 1) {
-                Log.d(TAG, "Alert 1");
-            } else if (loc == 2) {
-                Log.d(TAG, "Alert 2");
-            }
-            //} else {
-
-            //}
-
-        } catch (Exception e) {
-            //    e.printStackTrace();
-            Log.d(TAG, "Proximity Listener not Initialized ");
-            //Log.d(TAG, "Error in NSP, while attempting to send CP in UDP");
-        }
-    }
 
     public void skipSegment() {
         try {

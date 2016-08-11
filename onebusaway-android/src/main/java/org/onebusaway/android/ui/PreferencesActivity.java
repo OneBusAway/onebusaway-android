561cf812df60e7432ff21f7cf02d140d8d08d4d9
barbeau <barbeau@cutr.usf.edu>
2016-08-11 12:43:05 -0400

    Refactor action bar setup code into method

--- onebusaway-android/src/main/java/org/onebusaway/android/ui/PreferencesActivity.java
+++ onebusaway-android/src/main/java/org/onebusaway/android/ui/PreferencesActivity.java
@@ -192,49 +192,10 @@ public class PreferencesActivity extends PreferenceActivity
         ObaAnalytics.reportActivityStart(this);
     }
 
-
-    /**
-     * Imitate Action Bar with back button - from http://stackoverflow.com/a/27455363/937715
-     */
     @Override
     protected void onPostCreate(Bundle savedInstanceState) {
         super.onPostCreate(savedInstanceState);
-        Toolbar bar;
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
-            LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent()
-                    .getParent().getParent();
-            bar = (Toolbar) LayoutInflater.from(this)
-                    .inflate(R.layout.settings_toolbar, root, false);
-            root.addView(bar, 0); // insert at top
-        } else {
-            // For Gingerbread
-            ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
-            ListView content = (ListView) root.getChildAt(0);
-            root.removeAllViews();
-
-            bar = (Toolbar) LayoutInflater.from(this)
-                    .inflate(R.layout.settings_toolbar, root, false);
-
-            int height;
-            TypedValue tv = new TypedValue();
-            if (getTheme().resolveAttribute(R.attr.actionBarSize, tv, true)) {
-                height = TypedValue
-                        .complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
-            } else {
-                height = bar.getHeight();
-            }
-            content.setPadding(0, height, 0, 0);
-            root.addView(content);
-            root.addView(bar);
-        }
-
-        bar.setNavigationOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        setupActionBar();
     }
 
     @Override
@@ -489,6 +450,47 @@ public class PreferencesActivity extends PreferenceActivity
             apiUrl = getString(R.string.http_prefix) + apiUrl;
         }
         return Patterns.WEB_URL.matcher(apiUrl).matches();
+    }
+
+    /**
+     * Imitate Action Bar with back button - from http://stackoverflow.com/a/27455363/937715
+     */
+    private void setupActionBar() {
+        Toolbar bar;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            LinearLayout root = (LinearLayout) findViewById(android.R.id.list).getParent()
+                    .getParent().getParent();
+            bar = (Toolbar) LayoutInflater.from(this)
+                    .inflate(R.layout.settings_toolbar, root, false);
+            root.addView(bar, 0); // insert at top
+        } else {
+            // For Gingerbread
+            ViewGroup root = (ViewGroup) findViewById(android.R.id.content);
+            ListView content = (ListView) root.getChildAt(0);
+            root.removeAllViews();
+
+            bar = (Toolbar) LayoutInflater.from(this)
+                    .inflate(R.layout.settings_toolbar, root, false);
+
+            int height;
+            TypedValue tv = new TypedValue();
+            if (getTheme().resolveAttribute(R.attr.actionBarSize, tv, true)) {
+                height = TypedValue
+                        .complexToDimensionPixelSize(tv.data, getResources().getDisplayMetrics());
+            } else {
+                height = bar.getHeight();
+            }
+            content.setPadding(0, height, 0, 0);
+            root.addView(content);
+            root.addView(bar);
+        }
+
+        bar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
     }
 
     //

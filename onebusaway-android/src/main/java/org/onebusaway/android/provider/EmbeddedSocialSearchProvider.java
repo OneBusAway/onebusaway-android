2390e3944906a3cd9f470001d05dd16e4f01bd3f
Alex Crown <acrown@microsoft.com>
2017-02-17 17:53:00 -0800

    Removing unncessary Embedded Social search configurations

--- onebusaway-android/src/main/java/org/onebusaway/android/provider/EmbeddedSocialSearchProvider.java
+++
@@ -1,6 +0,0 @@
-package org.onebusaway.android.provider;
-
-import com.microsoft.embeddedsocial.provider.AbstractEmbeddedSocialSearchSuggestionProvider;
-
-public class EmbeddedSocialSearchProvider extends AbstractEmbeddedSocialSearchSuggestionProvider {
-}

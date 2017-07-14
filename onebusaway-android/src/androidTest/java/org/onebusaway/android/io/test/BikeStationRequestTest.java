339352e5d176be79c9f4cdf249c6b3c154799769
Rodrigo Carvalho <carvalhorr@gmail.com>
2017-07-14 17:26:10 +0100

    Added tests for all values of first bike station in the response.

--- onebusaway-android/src/androidTest/java/org/onebusaway/android/io/test/BikeStationRequestTest.java
+++ onebusaway-android/src/androidTest/java/org/onebusaway/android/io/test/BikeStationRequestTest.java
@@ -21,6 +21,22 @@ public class BikeStationRequestTest extends ObaTestCase {
         for (BikeRentalStation station: response.stations) {
             assertNotNull(station.name);
         }
+
+        assertFirstStationIsCorrect(response.stations.get(0));
+
+    }
+
+    private void assertFirstStationIsCorrect(BikeRentalStation station) {
+        double precision = 0.000001d;
+        assertEquals("\"bike_3566\"", station.id);
+        assertEquals("B-1165", station.name);
+        assertEquals(-82.40730666666667d, station.x, precision);
+        assertEquals(28.066505d, station.y, precision);
+        assertEquals(1, station.bikesAvailable);
+        assertEquals(0, station.spacesAvailable);
+        assertEquals(false, station.allowDropoff);
+        assertEquals(true, station.isFloatingBike);
+        assertEquals(true, station.realTimeData);
     }
 
 }

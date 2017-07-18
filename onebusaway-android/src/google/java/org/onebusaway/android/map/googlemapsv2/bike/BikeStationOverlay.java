2be73298595afad41c68fc995c9c4a6bf395831e
Rodrigo Carvalho <carvalhorr@gmail.com>
2017-07-18 23:03:12 +0100

    Fixed small error on bike marker info window.

--- onebusaway-android/src/google/java/org/onebusaway/android/map/googlemapsv2/bike/BikeStationOverlay.java
+++ onebusaway-android/src/google/java/org/onebusaway/android/map/googlemapsv2/bike/BikeStationOverlay.java
@@ -167,6 +167,7 @@ public class BikeStationOverlay
     @Override
     public void removeMarkerClicked(LatLng latLng) {
         mOnFocusChangedListener.onFocusChanged(null);
+        mBikeStationData.removeMarkerSelection();
     }
 
     private Bitmap createBitmapFromShape() {

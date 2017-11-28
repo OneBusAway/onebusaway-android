5b83e65d565ddc0f94b7731835946ec9bb5fed50
Alex Crown <acrown@microsoft.com>
2017-11-28 15:25:33 -0800

    Update copyright on files authored by Microsoft

--- onebusaway-android/src/main/java/org/onebusaway/android/report/ui/util/SocialReportHandler.java
+++ onebusaway-android/src/main/java/org/onebusaway/android/report/ui/util/SocialReportHandler.java
@@ -1,5 +1,5 @@
 /*
-* Copyright (C) 2014-2015 University of South Florida (sjbarbeau@gmail.com)
+* Copyright (c) 2017, Microsoft Corporation.
 *
 * Licensed under the Apache License, Version 2.0 (the "License");
 * you may not use this file except in compliance with the License.

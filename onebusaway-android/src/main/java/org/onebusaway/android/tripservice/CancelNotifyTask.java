7c1a8325c05c997538bd389ddc66df1e2a802cd9
paulnabanita1 <42182508+paulnabanita1@users.noreply.github.com>
2018-12-06 15:42:28 -0500

    Fix #934 - Modify TripService to start service in foreground (#937)
    * This will allow the service to execute after the device restarts so the reminders are rescheduled.  It has the side effect of always showing a notification when the service runs, so the user may briefly see extra notifications when reminders schedule, poll the server, notify, and after they are dismissed by the user.

--- onebusaway-android/src/main/java/org/onebusaway/android/tripservice/CancelNotifyTask.java
+++ onebusaway-android/src/main/java/org/onebusaway/android/tripservice/CancelNotifyTask.java
@@ -16,13 +16,13 @@
  */
 package org.onebusaway.android.tripservice;
 
-import org.onebusaway.android.provider.ObaContract;
-
 import android.content.ContentResolver;
 import android.content.ContentUris;
 import android.content.ContentValues;
 import android.content.Context;
 import android.net.Uri;
+
+import org.onebusaway.android.provider.ObaContract;
 
 /**
  * A task (thread) that is responsible for canceling a reminder.

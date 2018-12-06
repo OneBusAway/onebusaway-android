f26d0df9ad6a1b487bcacea5fa6f754f2325e34b
Sean Barbeau <sjbarbeau@gmail.com>
2018-12-06 18:21:45 -0500

    Check when app goes into the background

--- onebusaway-android/src/main/java/org/onebusaway/android/app/Application.java
+++ onebusaway-android/src/main/java/org/onebusaway/android/app/Application.java
@@ -16,6 +16,21 @@
  */
 package org.onebusaway.android.app;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.hardware.GeomagneticField;
+import android.location.Location;
+import android.location.LocationManager;
+import android.os.Build;
+import android.preference.PreferenceManager;
+import android.text.TextUtils;
+import android.util.Log;
+
 import com.google.android.gms.analytics.GoogleAnalytics;
 import com.google.android.gms.analytics.Tracker;
 import com.google.android.gms.common.ConnectionResult;
@@ -23,7 +38,6 @@ import com.google.android.gms.common.GoogleApiAvailability;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.tasks.Task;
-
 import com.microsoft.embeddedsocial.sdk.EmbeddedSocial;
 
 import org.onebusaway.android.BuildConfig;
@@ -40,27 +54,16 @@ import org.onebusaway.android.util.EmbeddedSocialUtils;
 import org.onebusaway.android.util.LocationUtils;
 import org.onebusaway.android.util.PreferenceUtils;
 
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.content.Context;
-import android.content.SharedPreferences;
-import android.content.pm.PackageInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.PackageManager.NameNotFoundException;
-import android.hardware.GeomagneticField;
-import android.location.Location;
-import android.location.LocationManager;
-import android.os.Build;
-import android.preference.PreferenceManager;
-import android.text.TextUtils;
-import android.util.Log;
-
 import java.security.MessageDigest;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.UUID;
 
+import androidx.lifecycle.Lifecycle;
+import androidx.lifecycle.LifecycleObserver;
+import androidx.lifecycle.OnLifecycleEvent;
+import androidx.lifecycle.ProcessLifecycleOwner;
 import androidx.multidex.MultiDexApplication;
 import edu.usf.cutr.open311client.Open311Manager;
 import edu.usf.cutr.open311client.models.Open311Option;
@@ -107,6 +110,19 @@ public class Application extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        ProcessLifecycleOwner.get().getLifecycle().addObserver(
+                new LifecycleObserver() {
+                    @OnLifecycleEvent(Lifecycle.Event.ON_START)
+                    void onStarted() {
+                        Log.d(TAG, "Started lifecycle");
+                    }
+
+                    @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
+                    void onStopped() {
+                        Log.d(TAG, "Stopped lifecycle");
+                    }
+                });
 
         mApp = this;
         mPrefs = PreferenceManager.getDefaultSharedPreferences(this);

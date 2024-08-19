93d0d365f7069dde74497da1923f0c2063485c7d
Amr Hossam <amrhossam.dev@gmail.com>
2024-08-19 22:21:04 +0300

    Implement saving of completed surveys to RoomD
    Signed-off-by: Amr Hossam <amrhossam.dev@gmail.com>

--- onebusaway-android/src/main/java/org/onebusaway/android/ui/survey/SurveyLocalData.java
+++ onebusaway-android/src/main/java/org/onebusaway/android/ui/survey/SurveyLocalData.java
@@ -11,49 +11,7 @@ import java.util.UUID;
 public class SurveyLocalData {
 
     private static final String PREFS_NAME = "survey_pref";
-    private static final String HASH_MAP_KEY = "survey_hash_map";
     private static final String UUID_KEY = "my_uuid";
-
-    /**
-     * Saves the answered surveys to SharedPreferences.
-     * The HashMap contains survey IDs as keys and their completion status as boolean values.
-     *
-     * @param context The context used to access SharedPreferences.
-     * @param hashMap A HashMap where the keys are survey IDs and the values are booleans indicating completion status.
-     */
-    public static void setCompletedSurveys(Context context, HashMap<String, Boolean> hashMap) {
-        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = prefs.edit();
-        JSONObject jsonObject = new JSONObject(hashMap);
-        String jsonString = jsonObject.toString();
-        editor.putString(HASH_MAP_KEY, jsonString);
-        editor.apply();
-    }
-
-    /**
-     * Retrieves the answered surveys from SharedPreferences as a HashMap.
-     * The map contains survey IDs as keys and their completion status as boolean values.
-     *
-     * @param context The context used to access SharedPreferences.
-     * @return A HashMap where the keys are survey IDs and the values are booleans indicating completion status.
-     */
-    public static HashMap<String, Boolean> getCompletedSurveys(Context context) {
-        SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
-        String jsonString = prefs.getString(HASH_MAP_KEY, "{}");
-        HashMap<String, Boolean> hashMap = new HashMap<>();
-        try {
-            JSONObject jsonObject = new JSONObject(jsonString);
-            Iterator<String> keys = jsonObject.keys();
-            while (keys.hasNext()) {
-                String key = keys.next();
-                boolean value = jsonObject.getBoolean(key);
-                hashMap.put(key, value);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-        return hashMap;
-    }
 
     public static void saveUserUUID(Context context, UUID uuid) {
         SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
---
+++ onebusaway-android/src/main/java/org/onebusaway/android/ui/survey/utils/SurveyDbHelper.kt
@@ -0,0 +1,51 @@
+package org.onebusaway.android.ui.survey.utils
+
+import android.content.Context
+import android.util.Log
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import org.onebusaway.android.io.request.survey.model.StudyResponse
+import org.onebusaway.android.ui.survey.entity.Study
+import org.onebusaway.android.ui.survey.entity.Survey
+import org.onebusaway.android.ui.survey.repository.SurveyRepository
+
+class SurveyDbHelper {
+    companion object {
+        private val coroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+        @JvmStatic
+        fun markSurveyAsCompleted(context: Context, survey: StudyResponse.Surveys) {
+            val surveyRepo = SurveyRepository(context)
+
+            val newStudy = Study(
+                survey.study.id, survey.study.name, survey.study.description, true
+            )
+            val newSurvey = Survey(survey.id, survey.study.id, survey.name)
+            coroutineScope.launch {
+                try {
+                    surveyRepo.addOrUpdateStudy(newStudy)
+                    surveyRepo.addSurvey(newSurvey)
+                    Log.d("All Saved Surveys", surveyRepo.getAllSurveys().toString());
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+        @JvmStatic
+        fun isSurveyCompleted(context: Context, surveyId: Int): Boolean {
+            val surveyRepo = SurveyRepository(context)
+            return runBlocking {
+                try {
+                    surveyRepo.checkSurveyCompleted(surveyId)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                    false
+                }
+            }
+        }
+    }
+}

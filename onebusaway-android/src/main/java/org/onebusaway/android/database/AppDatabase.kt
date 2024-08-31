8bfc981c2f50eca9396290b0e7d26e4ed553d5e9
Amr Hossam <amrhossam.dev@gmail.com>
2024-09-01 00:11:12 +0300

    chore: add comments
    Signed-off-by: Amr Hossam <amrhossam.dev@gmail.com>

--- onebusaway-android/src/main/java/org/onebusaway/android/database/AppDatabase.kt
+++ onebusaway-android/src/main/java/org/onebusaway/android/database/AppDatabase.kt
@@ -7,6 +7,11 @@ import org.onebusaway.android.ui.survey.dao.SurveysDao
 import org.onebusaway.android.ui.survey.entity.Study
 import org.onebusaway.android.ui.survey.entity.Survey
 
+/**
+ * Main database class for the app, containing `Study` and `Survey` entities.
+ * Provides abstract methods for accessing `StudiesDao` and `SurveysDao`.
+ * The `@Database` annotation sets up Room with version 1 of the schema.
+ */
 @Database(entities = [Study::class, Survey::class], version = 1)
 abstract class AppDatabase : RoomDatabase() {
     abstract fun studiesDao(): StudiesDao

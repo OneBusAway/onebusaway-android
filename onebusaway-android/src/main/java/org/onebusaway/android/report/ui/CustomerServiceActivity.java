e6d163364a66110aea2d3d967d5132f00c7c92d0
ShinLiX <xli20010110@gmail.com>
2026-03-13 11:37:09 -0700

    Resolved requested changes from reviewer

--- onebusaway-android/src/main/java/org/onebusaway/android/report/ui/CustomerServiceActivity.java
+++ onebusaway-android/src/main/java/org/onebusaway/android/report/ui/CustomerServiceActivity.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2015 University of South Florida (sjbarbeau@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.onebusaway.android.report.ui;
 
 import android.content.Context;
@@ -65,7 +80,7 @@ public class CustomerServiceActivity extends BaseReportActivity
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
 
-        setTitle(getString(R.string.rt_customer_service));
+        setTitle(R.string.rt_customer_service);
 
         mFirebaseAnalytics = FirebaseAnalytics.getInstance(this);
         mListView = findViewById(R.id.list);
@@ -82,6 +97,7 @@ public class CustomerServiceActivity extends BaseReportActivity
     public void onLoadFinished(Loader<ObaAgenciesWithCoverageResponse> loader,
                                ObaAgenciesWithCoverageResponse data) {
         if (data == null || data.getAgencies() == null) {
+            createToastMessage(getString(R.string.generic_comm_error));
             return;
         }
 

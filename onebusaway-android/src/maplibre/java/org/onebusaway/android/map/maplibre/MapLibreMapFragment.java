e227d1b00c9c10a406b4b7585bbbcc08beb0ae36
Aman Jain <aman.jain@devrev.ai>
2026-03-20 21:54:42 +0530

    refactor: extract duplicated inDarkMode() helper to UIUtils

--- onebusaway-android/src/maplibre/java/org/onebusaway/android/map/maplibre/MapLibreMapFragment.java
+++ onebusaway-android/src/maplibre/java/org/onebusaway/android/map/maplibre/MapLibreMapFragment.java
@@ -72,7 +72,6 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
-import android.content.res.Configuration;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.Bundle;
@@ -96,7 +95,6 @@ import java.util.concurrent.TimeUnit;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.drawable.DrawableCompat;
 import androidx.fragment.app.DialogFragment;
 
@@ -235,11 +233,7 @@ public class MapLibreMapFragment extends SupportMapFragment
     }
 
     private boolean inDarkMode() {
-        return AppCompatDelegate.getDefaultNightMode() == AppCompatDelegate.MODE_NIGHT_YES || (
-                AppCompatDelegate.getDefaultNightMode() != AppCompatDelegate.MODE_NIGHT_NO &&
-                        (getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK)
-                                == Configuration.UI_MODE_NIGHT_YES
-        );
+        return UIUtils.inDarkMode(getResources());
     }
 
     @Override

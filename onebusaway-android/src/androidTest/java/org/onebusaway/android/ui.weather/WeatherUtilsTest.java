28674a3cda0ed1fa410dfd6229b7fb7476533f6a
Alex George <122389120+ag10889@users.noreply.github.com>
2026-05-04 12:54:25 -0400

    Add WeatherUtilsTest class with initial test method

---
+++ onebusaway-android/src/androidTest/java/org/onebusaway/android/ui.weather/WeatherUtilsTest.java
@@ -0,0 +1,16 @@
+package org.onebusaway.android.ui.weather;
+import android.widget.ImageView;
+import org.junit.Test;
+import org.onebusaway.android.ui.weather.WeatherUtils;
+
+import static org.junit.Assert.assertEquals;
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class WeatherUtilsTest {
+
+   @Test
+    public void SetWeatherImage(){
+
+   }
+}

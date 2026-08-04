a202fa8f0d1f7b99be3a276ed5905e7cb9b9a856
Brandon Martin-Anderson <badhill@gmail.com>
2026-08-04 00:33:31 -0700

    Fix transit header focus test

--- onebusaway-android/src/androidTest/java/org/onebusaway/android/ui/tripresults/DirectionRowFocusTest.kt
+++ onebusaway-android/src/androidTest/java/org/onebusaway/android/ui/tripresults/DirectionRowFocusTest.kt
@@ -165,7 +165,7 @@ class DirectionRowFocusTest {
         composeRule.onNodeWithText(midStopName).assertDoesNotExist()
 
         // Tapping the transit header highlights the route but does not reveal the stops (#2040).
-        composeRule.onNodeWithText(transitTitle).performClick()
+        composeRule.onNodeWithText(requireNotNull(transit.headsign)).performClick()
         assertEquals("1_100", captured?.first?.routeId)
         assertEquals(FocusedLeg(transitLegPoints, setOf(1)), captured?.second)
         composeRule.onNodeWithText(midStopName).assertDoesNotExist()
